a403c02d385fc711317d96e0ac3b370f1db0fd0e
mitcmurph <mitcmurph@github.com>
2020-04-06 22:03:31 +1000

    clean file

--- backend/src/routes/circuit-router.js
+++ backend/src/routes/circuit-router.js
@@ -1,11 +1,8 @@
 import { Router } from "express";
 import * as circuitController from "../controllers/circuit";
-// const Router = require("express");
-// const circuitController = require("../controllers/circuit");
 
 const circuitRouter = Router();
 
 circuitRouter.post("/solve", circuitController.solve);
 
-export default circuitRouter;
-// module.exports = circuitRouter;
+module.exports = circuitRouter;
