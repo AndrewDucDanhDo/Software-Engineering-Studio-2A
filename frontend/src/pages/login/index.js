bef840d3c9283840d67900d163a5c70d3959f4c1
Agid-Kaharuba <razinagida@gmail.com>
2020-04-18 01:57:01 +1000

    Made an example for login

--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import {loginUser} from "../../helpers/auth";
 import {withStyles} from "@material-ui/styles";
+import {TextField} from "@material-ui/core";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+import Grid from "@material-ui/core/Grid";
 
 const styles = {
 	test : {
@@ -59,36 +63,39 @@ export class LoginPage extends React.Component {
 
 	loginForm = () => {
 		return (
-			
-			<div className={this.props.classes.test}>
-				<h2>Im the login page</h2>
-				<form onSubmit={this.handleLogin}>
-					{/* email */}
-					<div>
-						<label>Email</label>
-						<input
-							type="text"
-							placeholder="Email"
-							name="email"
-							required
-							onChange={this.handleFormChange}
-						/>
-					</div>
-					{/* password */}
-					<div>
-						<label>Password</label>
-						<input
-							type="text"
-							placeholder="Password"
-							name="password"
-							required
-							onChange={this.handleFormChange}
-						/>
-					</div>
+			<Grid container justify="center" alignItems="center">
+				<div className={this.props.classes.test}>
+					<h2>Im the login page</h2>
+					<form onSubmit={this.handleLogin}>
+						{/* email */}
+						<div>
+							<label>Email</label>
+							{/*<input*/}
+							{/*	type="text"*/}
+							{/*	placeholder="Email"*/}
+							{/*	name="email"*/}
+							{/*	required*/}
+							{/*	onChange={this.handleFormChange}*/}
+							{/*/>*/}
+							<TextField label="email" variant="outlined" onChange={this.handleFormChange}/>
+						</div>
+						{/* password */}
+						<div>
+							<label>Password</label>
+							{/*<input*/}
+							{/*	type="text"*/}
+							{/*	placeholder="Password"*/}
+							{/*	name="password"*/}
+							{/*	required*/}
+							{/*	onChange={this.handleFormChange}*/}
+							{/*/>*/}
+							<TextField label="password" variant="outlined" onChange={this.handleFormChange}/>
+						</div>
 
-					<button type="Login">Submit</button>
-				</form>
-			</div>
+						<button type="Login">Submit</button>
+					</form>
+				</div>
+			</Grid>
 		);
 	};
 
