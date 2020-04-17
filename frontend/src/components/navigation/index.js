07beafb493a79da484529e1b8276f72aa024ec79
Andy <andyau99@gmail.com>
2020-04-17 14:51:46 +1000

    Collab with Agid to fix navbar
    added box and color.

--- frontend/src/components/navigation/index.js
+++ frontend/src/components/navigation/index.js
@@ -6,6 +6,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -15,7 +16,8 @@ const useStyles = makeStyles((theme) => ({
 	  marginRight: theme.spacing(3),
 	},
 	title: {
-	  flexGrow: 1,
+	  color: 'white',
+	  textDecoration: 'none',
 	},
 	navcolor: {
 		background : 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
@@ -35,15 +37,16 @@ function Navigation() {
 			<div className="App container">
 				<AppBar className={classes.navcolor} position="static">
 					<Toolbar>
+						<Box flexGrow={ 1 }>
 						<Typography variant="h5" className={classes.title} component={Link} to="/">
 							Quantum Circuit Simulator Group 2
 						</Typography>
+						</Box>
 							<Button className={classes.signup} color="inherit" component={Link} to="/signup"> Sign Up</Button>
 							<Button color="inherit" component={Link} to="/login">Login</Button>
 					</Toolbar>
 				</AppBar>
 			</div>
-
 	);
 }
 
