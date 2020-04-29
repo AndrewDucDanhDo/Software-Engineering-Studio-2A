0fe7d6832062f4f364b74dc368a0ce237bf61c30
mitcmurph <mitcmurph@gmail.com>
2020-04-29 23:09:43 +1000

    save circuit POC

--- backend/src/routes/circuit-router.js
+++ backend/src/routes/circuit-router.js
@@ -4,5 +4,6 @@ import * as circuitController from "../controllers/circuit";
 const circuitRouter = Router();
 
 circuitRouter.post("/solve", circuitController.solve);
+circuitRouter.post("/save", circuitController.saveCircuit);
 
 export default circuitRouter;
