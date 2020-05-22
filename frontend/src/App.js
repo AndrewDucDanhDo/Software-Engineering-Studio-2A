82595ca3e0c2011d19196691f32bc7eb067471e5
Agid-Kaharuba <razinagida@gmail.com>
2020-05-23 03:57:08 +1000

    Reorganized App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,9 @@ import React from "react";
 import "./styles/App.css";
 import { ThemeProvider } from "@material-ui/core/styles";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-
 // Components
 import Navigation from "./components/navigation";
 import Footer from "./components/footer";
-
 // Pages
 import SignupPage from "./pages/signup";
 import LoginPage from "./pages/login";
@@ -16,62 +14,75 @@ import appTheme from "./helpers/appTheme";
 import TeacherTaskEditorPage from "./pages/teacherTaskEditor";
 import TeacherTasksPage from "./pages/teacherTasks";
 import TeacherTaskViewerPage from "./pages/teacherTaskViewer";
-
 // Context
 import { AuthProvider } from "./context/auth";
 import PrivateRoute from "./components/PrivateRoute";
 
+function AppProvider(props) {
+	return (
+		<ThemeProvider theme={appTheme}>
+			<AuthProvider>
+				{props.children}
+			</AuthProvider>
+		</ThemeProvider>
+	);
+}
+
+function AppRouter(props) {
+	return (
+		<Switch>
+			<PrivateRoute
+				path="/teacherTasks"
+				component={TeacherTasksPage}
+				adminRoute={true}
+			/>
+
+			<PrivateRoute
+				path="/teacherTaskViewer"
+				component={TeacherTaskViewerPage}
+				adminRoute={true}
+			/>
+
+			<PrivateRoute
+				path="/teacherTaskEditor"
+				component={TeacherTaskEditorPage}
+				adminRoute={true}
+			/>
+
+			<PrivateRoute
+				path="/profile"
+				adminRoute={false}
+				component={ProfilePage}
+			/>
+
+			<Route path="/signup">
+				<SignupPage />
+			</Route>
+
+			<Route path="/login">
+				<LoginPage />
+			</Route>
+
+			<Route path="/">
+				<HomePage />
+			</Route>
+		</Switch>
+	);
+}
+
 function App() {
 	return (
-		<AuthProvider>
-			<div className="App">
-				<ThemeProvider theme={appTheme}>
-					<BrowserRouter>
-						<Navigation />
-						<div>
-							<Switch>
-								<PrivateRoute
-									path="/teacherTasks"
-									component={TeacherTasksPage}
-									adminRoute={true}
-								/>
-
-								<PrivateRoute
-									path="/teacherTaskViewer"
-									component={TeacherTaskViewerPage}
-									adminRoute={true}
-								/>
-
-								<PrivateRoute
-									path="/teacherTaskEditor"
-									component={TeacherTaskEditorPage}
-									adminRoute={true}
-								/>
-
-								<PrivateRoute
-									path="/profile"
-									adminRoute={false}
-									component={ProfilePage}
-								/>
-
-								<Route path="/signup">
-									<SignupPage />
-								</Route>
-
-								<Route path="/login">
-									<LoginPage />
-								</Route>
-
-								<Route path="/">
-									<HomePage />
-								</Route>
-							</Switch>
-							<Footer />
-						</div>
-					</BrowserRouter>
-				</ThemeProvider>
-			</div>
-		</AuthProvider>
+		<div className="App">
+			<AppProvider>
+				<BrowserRouter>
+					<Navigation />
+					<div>
+						<AppRouter/>
+						<Footer />
+					</div>
+				</BrowserRouter>
+			</AppProvider>
+		</div>
 	);
 }
 
