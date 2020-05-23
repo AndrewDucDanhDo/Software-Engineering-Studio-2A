8325230e94e57821db87d60283e0ec67b21d0a20
aaron <aaron.abm@gmail.com>
2020-05-24 00:11:55 +1000

    ignoring package-lock

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,19 +1,30 @@
 import React from "react";
-import {Box, Button, Grid, Paper} from "@material-ui/core";
-import QuantumSimulator from "../../components/quantum";
+import { Grid, Paper } from "@material-ui/core";
+import QuCircuit from "../../components/quCircuit";
+
 function HomePage() {
 	return (
-		<Grid container style={{position: "absolute", width: "100%", height: "90%"}}>
-                <Grid xs={2} item component={Paper} style={{backgroundColor: "#f7f7f7"}}>
-                <div style={{textAlign: "center"}}>
-                        <h2>Advertise here!</h2>
-                        <a href="https://www.youtube.com/watch?v=oT3mCybbhf0" target="_blank">Click me for more info</a>
-                </div>
-                </Grid>
-                <Grid xs={10} item>
-                    <QuantumSimulator/>
-                </Grid>
-        </Grid>
+		<Grid
+			container
+			style={{ position: "absolute", width: "100%", height: "90%" }}
+		>
+			<Grid
+				xs={2}
+				item
+				component={Paper}
+				style={{ backgroundColor: "#f7f7f7" }}
+			>
+				<div style={{ textAlign: "center" }}>
+					<h2>Advertise here!</h2>
+					<a href="https://www.youtube.com/watch?v=oT3mCybbhf0" target="_blank">
+						Click me for more info
+					</a>
+				</div>
+			</Grid>
+			<Grid xs={10} item>
+				<QuCircuit />
+			</Grid>
+		</Grid>
 	);
 }
 
