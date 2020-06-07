9fd07883dfc79a54f6043f02b49cad98504aa623
mitcmurph <mitcmurph@gmail.com>
2020-06-07 15:35:23 +1000

    run student submission on backend

--- backend/src/routes/task-admin-router.js
+++ backend/src/routes/task-admin-router.js
@@ -14,6 +14,7 @@ import {
   updateSubmissionResults,
   markTaskSubmissions,
   markUserSubmission,
+  runUserSubmission,
   resetTaskResults
 } from "../controllers/submission";
 
@@ -30,5 +31,6 @@ taskAminRouter.post("/:taskId/submission/:userId/update", updateSubmissionResult
 taskAminRouter.post("/:taskId/mark", markTaskSubmissions);
 taskAminRouter.post("/:taskId/mark/reset", resetTaskResults);
 taskAminRouter.post("/:taskId/submission/:userId/mark", markUserSubmission);
+taskAminRouter.get("/:taskId/submission/:userId/mark", runUserSubmission);
 
 export default taskAminRouter;
